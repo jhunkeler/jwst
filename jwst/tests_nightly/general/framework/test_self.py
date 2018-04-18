8dcab045fe60eacdf443481cfd45aa3cef5916b5
Joseph Hunkeler <jhunkeler@gmail.com>
2018-04-18 17:07:28 -0400

    Streamline self-test

--- jwst/tests_nightly/general/framework/test_self.py
+++ jwst/tests_nightly/general/framework/test_self.py
@@ -1,18 +1,26 @@
+import os
 import pytest
 
+from jwst.helpers.io import get_bigdata
 
-pytestmark = [
-    pytest.mark.usefixtures('_jail'),
-    pytest.mark.skipif(not pytest.config.getoption('bigdata'),
+
+@pytest.mark.usefixtures('_jail')
+class TestBigdata:
+    _starting_path = os.path.abspath(os.curdir)
+
+    @pytest.mark.skipif(not pytest.config.getoption('bigdata'),
                        reason='requires --bigdata')
-]
+    def test_bigdata(self):
+        """ Test ability to retrieve big data from a list of
+        predefined resources (defined in jwst.helpers.io).
+        """
+        filename = get_bigdata('helpers', 'alive', 'alive-0.txt')
+        assert os.path.exists(filename)
+        assert os.stat(filename).st_size
+
+    def test_jail(self):
+        """ Ensures the current working directory has been changed
+        """
+        assert os.path.abspath(os.curdir) != self._starting_path
 
 
-def test_bigdata(_bigdata):
-    with open('bigdata.txt', 'w+') as fp:
-        fp.write(_bigdata + '\n')
-
-
-def test_jail(_bigdata):
-    with open('output.txt', 'w+') as fp:
-        fp.write('hello world!\n')
