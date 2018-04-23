be9df16f12c7a8ca86712b93393bc1cf0a2063de
Joseph Hunkeler <jhunkeler@gmail.com>
2018-04-23 10:14:46 -0400

    Simplify a miri test

--- jwst/tests_nightly/miri/test_bias_drift2.py
+++ jwst/tests_nightly/miri/test_bias_drift2.py
@@ -14,25 +14,13 @@ def test_refpix_miri2():
     Regression test of refpix step performed on MIRI data.
 
     """
+    input_file = get_bigdata('miri/test_bias_drift/jw00025001001_01107_00001_MIRIMAGE_saturation.fits')
+    ref_file = get_bigdata('miri/test_bias_drift/jw00025001001_01107_00001_MIRIMAGE_bias_drift.fits')
     output_file_base, output_file = add_suffix('refpix2_output.fits', 'refpix')
+    diff_keys = ['primary', 'sci', 'err', 'pixeldq', 'groupdq']
 
-    try:
-        os.remove(output_file)
-    except:
-        pass
-
-
-
-    RefPixStep.call(_bigdata+'/miri/test_bias_drift/jw00025001001_01107_00001_MIRIMAGE_saturation.fits',
+    RefPixStep.call(input_file,
                     use_side_ref_pixels=False, side_smoothing_length=10, side_gain=1.0,
                     output_file=output_file_base)
-    h = fits.open(output_file)
-    href = fits.open(_bigdata+'/miri/test_bias_drift/jw00025001001_01107_00001_MIRIMAGE_bias_drift.fits')
-    newh = fits.HDUList([h['primary'],h['sci'],h['err'],h['pixeldq'],h['groupdq']])
-    newhref = fits.HDUList([href['primary'],href['sci'],href['err'],href['pixeldq'],href['groupdq']])
-    result = fits.diff.FITSDiff(newh,
-                              newhref,
-                              ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
-                              rtol = 0.00001
-    )
-    assert result.identical, result.report()
+
+    cmp_fitshdr(output_file, ref_file, diff_keys)
