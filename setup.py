211dabef3a7c2b95059ae2b0eae3a7eb02626cc8
Joseph Hunkeler <jhunkeler@gmail.com>
2018-06-21 11:52:22 -0400

    Only GOD knows

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
+import fnmatch
+import importlib
 import os
-from os.path import basename
-import subprocess
+import pkgutil
+import platform
 import sys
+from distutils.spawn import find_executable
+from glob import glob
+from os.path import basename
 from setuptools import setup, find_packages, Extension, Command
 from setuptools.command.test import test as TestCommand
-from numpy import get_include as np_include
-from glob import glob
+from subprocess import check_call, CalledProcessError
+
+try:
+    from numpy import get_include as np_include
+except ImportError:
+    print('numpy is missing, please install it.')
+    exit(1)
 
 # hack building the sphinx docs with C source
 from setuptools.command.build_ext import build_ext
+
 
 if sys.version_info < (3, 5):
     error = """
@@ -72,7 +83,38 @@ PACKAGE_DATA = {
         '*.json'
     ]
 }
-
+INSTALL_REQUIRES=[
+    'asdf',
+    'astropy',
+    'crds',
+    'drizzle',
+    'gwcs',
+    'jsonschema',
+    'namedlist',
+    'numpy',
+    'scipy',
+    'spherical-geometry',
+    'six',
+    'stsci.tools',
+    'stsci.image',
+    'stsci.imagestats',
+    'stsci.convolve',
+    'stsci.stimage',
+    'photutils',
+    'verhawk',
+]
+EXTRAS_REQUIRE={
+    'ephem': ['pymssql', 'jplephem'],
+}
+TESTS_REQUIRE=[
+    'pytest',
+    'pytest-remotedata',
+    'requests_mock',
+]
+EXTERN_BIN_REQUIRES = {
+        'freetds': 'tsql',
+        'unixodbc': 'odbcinst',
+}
 
 def get_transforms_data():
     # Installs the schema files in jwst/transforms
@@ -117,23 +159,108 @@ class PyTest(TestCommand):
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
+def find_library(name):
+    if not name:
+        return None
 
-if os.path.exists('relic'):
-    sys.path.insert(1, 'relic')
-    import relic.release
-else:
+    prefix = sys.prefix
+    dynamic_link_env = [
+        # Linux
+        'LD_LIBRARY_PATH',
+        # Darwin
+        'DYLD_LIBRARY_PATH',
+        'DYLD_FALLBACK_LIBRARY_PATH',
+        # Windows
+        'LIBRARY_PATH',
+        'PATH'
+    ]
+    libexts = [
+        'so',
+        'dylib',
+        'dll',
+    ]
+    libdirs = [
+        os.path.join(prefix, 'lib'),
+        os.path.join(prefix, 'lib64'),
+    ]
+
+    # Search for shared libraries beyond Python's installation prefix
+    for dlvar in dynamic_link_env:
+        result = os.environ.get(dlvar, '')
+        if result:
+            libdirs += result.split(os.pathsep)
+
+    if not name.startswith('lib'):
+        name = ''.join(['lib', name])
+
+    for libdir in [x for x in libdirs if os.path.exists(x)]:
+        for libext in libexts:
+            ext = '*.'.join(['', libext])
+            for root, dirs, files in os.walk(libdir):
+                for fname in files:
+                    fname_no_ext = fname.split('.', 1)[0]
+                    if fname_no_ext == name:
+                        if fnmatch.fnmatch(fname, ext):
+                            return root, \
+                                   os.path.basename(fname), \
+                                   fname_no_ext
+    return None
+
+
+def summary():
+    from verhawk.scanner import Scanner
+
+    fmt = '    {:.<30s}{:<10} (version: {})\n'
+    vh = None
+    output = '\n\n========= Build Summary =========\n\n'
+
+    # Python packages
+    output += '[REQUIRED] Dependencies:\n\n'
+    for package in sorted(INSTALL_REQUIRES):
+        status = 'Missing'
+        package = package.replace('-', '_')
+        spec = importlib.util.find_spec(package)
+
+        if spec:
+            module = importlib.util.module_from_spec(spec)
+            try:
+                spec.loader.exec_module(module)
+                sys.modules[package] = module
+            except (AttributeError, TypeError):
+                pass
+
+            vh = Scanner(module).versions
+            status = 'Installed'
+
+        output += fmt.format(package, status, vh[package] or 'Unknown')
+
+    output += '\n[OPTIONAL] Dependencies:\n\n'
+
+    # Binaries
+    fmt = '    {:.<30s}{:<10}\n'
+    for key, value in EXTERN_BIN_REQUIRES.items():
+        status = find_executable(value) == 'Installed' or 'Missing'
+        output += fmt.format(key, status)
+
+    print(output)
+
+if not pkgutil.find_loader('relic'):
+    relic_local = os.path.exists('relic')
+    relic_submodule = (relic_local and
+                       os.path.exists('.gitmodules') and
+                       not os.listdir('relic'))
     try:
-        import relic.release
-    except ImportError:
-        try:
-            subprocess.check_call(['git', 'clone',
-                'https://github.com/jhunkeler/relic.git'])
-            sys.path.insert(1, 'relic')
-            import relic.release
-        except subprocess.CalledProcessError as e:
-            print(e)
-            exit(1)
+        if relic_submodule:
+            check_call(['git', 'submodule', 'update', '--init', '--recursive'])
+        elif not relic_local:
+            check_call(['git', 'clone', 'https://github.com/spacetelescope/relic.git'])
 
+        sys.path.insert(1, 'relic')
+    except CalledProcessError as e:
+        print(e)
+        exit(1)
+
+import relic.release
 
 version = relic.release.get_info()
 relic.release.write_template(version, NAME)
@@ -161,20 +288,18 @@ setup(
     package_data=PACKAGE_DATA,
     ext_modules=[
         Extension('jwst.tweakreg.chelp',
-            glob('src/tweakreg/*.c'),
-            include_dirs=[np_include()],
-            define_macros=[('NUMPY', '1')]),
+                  glob('src/tweakreg/*.c'),
+                  include_dirs=[np_include()],
+                  define_macros=[('NUMPY', '1')]),
     ],
-    install_requires=[
-        'namedlist'
-    ],
-    tests_require=[
-        'pytest',
-        'requests_mock'
-    ],
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    tests_require=TESTS_REQUIRE,
     cmdclass={
         'test': PyTest,
         'build_sphinx': BuildSphinx
     },
     entry_points=entry_points,
 )
+
+summary()
